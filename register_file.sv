e54df46204592108ba2c28ca7aff8df4701de0d4
TilesOS <tribeleon42@gmail.com>
2026-06-18 21:11:03 -0400

    first write done

--- register_file.sv
+++ register_file.sv
@@ -1,17 +1,20 @@
 module register_file #(
     parameter int DATA_WIDTH = 32,
     parameter int NUM_REGS = 32,
-    parameter int ADDR_WIDTH = 5
+    parameter int ADDR_WIDTH = 5 // 2^{5} = 32
 )(
     input logic clk,
-    input logic n_rst,
+    input logic rstn, // reset on negedge
 
+    // read port 1
     input logic [ADDR_WIDTH-1:0] rs1_addr,
     input logic [DATA_WIDTH-1:0] rs1_data,
 
+    // read port 2
     input logic [ADDR_WIDTH-1:0] rs2_addr,
     input logic [DATA_WIDTH-1:0] rs2_data,
 
+    // write port
     input logic we,
     input logic [ADDR_WIDTH-1:0] rd_addr,
     input logic [DATA_WIDTH-1:0] rd_data
@@ -19,10 +22,19 @@ module register_file #(
 
     logic [DATA_WIDTH-1:0] rf [NUM_REGS];
 
+    // reads asynchronously
     assign rs1_data = (rs1_addr == '0) ? '0 : rf [rs1_addr];
     assign rs2_data = (rs2_addr == '0) ? '0 : rf [rs2_addr];
 
-    always_ff @ (posedge clk)
-    
+    // writes synchronously
+    always_ff @ (posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            for (int i = 0; i < NUM_REGS; i++) begin
+                rf [i] <= '0;
+            end
+        end else if (we && (rd_addr != '0)) begin
+            rf [rd_addr] <= rd_data;
+        end
+    end
 
 endmodule
