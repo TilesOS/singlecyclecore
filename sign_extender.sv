79afd2260d9b14359722c93ca495aed3f5eb7074
TilesOS <tribeleon42@gmail.com>
2026-06-21 19:20:34 -0400

    sign extender module

--- sign_extender.sv
+++ sign_extender.sv
@@ -1,3 +1,11 @@
-module sign_extender (
-    
-)
+module sign_extender #(
+    parameter int IN_WIDTH  = 12,
+    parameter int OUT_WIDTH = 32
+)(
+    input  logic [IN_WIDTH-1:0]  in,
+    output logic [OUT_WIDTH-1:0] out
+);
+
+    assign out = {{(OUT_WIDTH-IN_WIDTH){in[IN_WIDTH-1]}}, in};
+
+endmodule
