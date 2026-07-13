6864efa8f4ef2d3ae9c3216a5e48b3333d72e7e3
Tyler McClure <tylermcclure11@gmail.com>
2026-07-13 19:43:02 -0400

    almost forgot alu package

---
+++ alu_pkg.sv
@@ -0,0 +1,19 @@
+package alu_pkg;
+
+    parameter int DATA_WIDTH = 32;
+    parameter int SHIFT_WIDTH = 5;
+
+    typedef enum logic [3:0] {
+        ALU_ADD,
+        ALU_SUB,
+        ALU_AND,
+        ALU_OR,
+        ALU_XOR,
+        ALU_SLL,
+        ALU_SRL,
+        ALU_SRA,
+        ALU_SLT,
+        ALU_SLTU
+    } alu_op_t;
+
+endpackage
